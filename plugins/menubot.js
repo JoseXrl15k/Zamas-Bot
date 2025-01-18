63bb9afa7cbf965ae2c7594ab3f253f8d8067db2
Jose Elber <xrljose105@gmail.com>
2025-01-18 17:19:33 -0500

    Update menubot.js

--- plugins/menubot.js
+++ plugins/menubot.js
@@ -4,6 +4,369 @@ let texto = `
 *\`¡Bienvenido a Zamas Bot! Este innovador bot de WhatsApp, creado por José, está diseñado para ofrecerte herramientas útiles, entretenimiento y acceso a diversas funciones de manera rápida y sencilla.\`*
 
 *\`Nuestro compromiso es proporcionarte un servicio de alta calidad, y estamos en constante desarrollo para mejorar la experiencia de nuestros usuarios. Agradecemos tu confianza y esperamos que disfrutes de todas las funcionalidades que Zamas Bot tiene para ofrecerte. ¡Gracias por ser parte de esta experiencia!\`*
+
+ ╭══════════════ ⪩
+╭┤き⃟🔥 𝐈𝐍𝐅𝐎 𝐁𝐎𝐓 ✰⃟⃟き
+┃│ 🌩 .formarpareja5
+┃│ 🌩 .estado
+┃│ 🌩 .runtime
+┃│ 🌩 .script
+┃│ 🌩 .staff
+┃│ 🌩 .blocklist
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐁𝐔𝐒𝐂𝐀𝐃𝐎𝐑𝐄𝐒 ✰⃟⃟き
+┃│ 🌩 .githubsearch
+┃│ 🌩 .google <búsqueda>
+┃│ 🌩 .mercadolibre <búsqueda>
+┃│ 🌩 .imagen <query>
+┃│ 🌩 .pinterest
+┃│ 🌩 .movistar
+┃│ 🌩 .githubstalk
+┃│ 🌩 .playstore 
+┃│ 🌩 .pelisplus
+┃│ 🌩 .minestalk
+┃│ 🌩 .acordes
+┃│ 🌩 .mcmods
+┃│ 🌩 .tiktoksearch <txt>
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐉𝐔𝐄𝐆𝐎𝐒 ✰⃟⃟き
+┃│ 🌩 .69 @tag
+┃│ 🌩 .abrazar <@usuario>
+┃│ 🌩 .acertijo
+┃│ 🌩 .agarrar @tag
+┃│ 🌩 .anal @tag
+┃│ 🌩 .sonrojarse @tag
+┃│ 🌩 .gay <@tag> | <nombre>
+┃│ 🌩 .lesbiana <@tag> | <nombre>
+┃│ 🌩 .pajero <@tag> | <nombre>
+┃│ 🌩 .pajera <@tag> | <nombre>
+┃│ 🌩 .puto <@tag> | <nombre>
+┃│ 🌩 .puta <@tag> | <nombre>
+┃│ 🌩 .manco <@tag> | <nombre>
+┃│ 🌩 .manca <@tag> | <nombre>
+┃│ 🌩 .rata <@tag> | <nombre>
+┃│ 🌩 .prostituta <@tag> | <nombre>
+┃│ 🌩 .prostituto <@tag> | <nombre>
+┃│ 🌩 .apostar *<cantidad>*
+┃│ 🌩 .chupartetas @tag
+┃│ 🌩 .consejo
+┃│ 🌩 .cum @tag
+┃│ 🌩 .dance *<@user>*
+┃│ 🌩 .formarpareja5
+┃│ 🌩 .abrazar @tag
+┃│ 🌩 .violar @tag
+┃│ 🌩 .dormir @tag
+┃│ 🌩 .lamber @tag
+┃│ 🌩 .enamorada @tag
+┃│ 🌩 .mamada @tag
+┃│ 🌩 .meme
+┃│ 🌩 .violar @tag
+┃│ 🌩 .nombreninja *<texto>*
+┃│ 🌩 .acariciar @tag
+┃│ 🌩 .penetrar @user
+┃│ 🌩 .personalidad
+┃│ 🌩 .piropo
+┃│ 🌩 .pokedex *<pokemon>*
+┃│ 🌩 .pucheros @tag
+┃│ 🌩 .pregunta
+┃│ 🌩 .golpear @tag
+┃│ 🌩 .reto
+┃│ 🌩 .ruleta *<cantidad> <color>*
+┃│ 🌩 .rusa @tag
+┃│ 🌩 .triste @tag
+┃│ 🌩 .scared @tag
+┃│ 🌩 .sexo @tag
+┃│ 🌩 .ship
+┃│ 🌩 .love
+┃│ 🌩 .timida @tag
+┃│ 🌩 .simi
+┃│ 🌩 .bot
+┃│ 🌩 .dormir @tag
+┃│ 🌩 .dormir @tag
+┃│ 🌩 .top *<texto>*
+┃│ 🌩 .violar @tag
+┃│ 🌩 .tijeras @tag
+┃│ 🌩 .zodiac *2002 02 25*
+┃│ 🌩 .cancion
+┃│ 🌩 .math <mode>
+┃│ 🌩 .ppt
+┃│ 🌩 .slot <apuesta>
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐒𝐄𝐑𝐁𝐎𝐓 ✰⃟⃟き
+┃│ 🌩 .jadibot
+┃│ 🌩 .serbot
+┃│ 🌩 .bots
+┃│ 🌩 .deletebot
+┃│ 🌩 .pausarai
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥  𝐁 𝐄 𝐌 ✰⃟⃟き
+┃│ 🌩 .bank
+┃│ 🌩 .cookies
+┃│ 🌩 .crimen
+┃│ 🌩 .daily
+┃│ 🌩 .claim
+┃│ 🌩 .depositar
+┃│ 🌩 .lb
+┃│ 🌩 .levelup
+┃│ 🌩 .minar
+┃│ 🌩 .retirar
+┃│ 🌩 .rob2
+┃│ 🌩 .rob
+┃│ 🌩 .addprem [@user] <days>
+┃│ 🌩 .slut
+┃│ 🌩 .trabajar
+┃│ 🌩 .transfer [tipo] [cantidad] [@tag]
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 ✰⃟⃟き
+┃│ 🌩 .profile
+┃│ 🌩 .unreg
+┃│ 🌩 .reg
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥  𝐄 𝐗 𝐏 ✰⃟⃟き
+┃│ 🌩 .bal
+┃│ 🌩 .daily
+┃│ 🌩 .Buy
+┃│ 🌩 .Buyall
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒 ✰⃟⃟き
+┃│ 🌩 .toimg (reply)
+┃│ 🌩 .qc
+┃│ 🌩 .stiker <img>
+┃│ 🌩 .sticker <url>
+┃│ 🌩 .wm <packname>|<author>
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐀𝐍𝐈𝐌𝐄𝐒✰⃟⃟き
+┃│ 🌩 .animelink
+┃│ 🌩 .akira
+┃│ 🌩 .akiyama
+┃│ 🌩 .anna
+┃│ 🌩 .asuna
+┃│ 🌩 .ayuzawa
+┃│ 🌩 .boruto
+┃│ 🌩 .chiho
+┃│ 🌩 .chitoge
+┃│ 🌩 .deidara
+┃│ 🌩 .erza
+┃│ 🌩 .elaina
+┃│ 🌩 .eba
+┃│ 🌩 .emilia
+┃│ 🌩 .hestia
+┃│ 🌩 .hinata
+┃│ 🌩 .inori
+┃│ 🌩 .isuzu
+┃│ 🌩 .itachi
+┃│ 🌩 .itori
+┃│ 🌩 .kaga
+┃│ 🌩 .kagura
+┃│ 🌩 .kaori
+┃│ 🌩 .keneki
+┃│ 🌩 .kotori
+┃│ 🌩 .kurumi
+┃│ 🌩 .madara
+┃│ 🌩 .mikasa
+┃│ 🌩 .miku
+┃│ 🌩 .minato
+┃│ 🌩 .naruto
+┃│ 🌩 .nezuko
+┃│ 🌩 .sagiri
+┃│ 🌩 .sasuke
+┃│ 🌩 .sakura
+┃│ 🌩 .cosplay
+┃│ 🌩 .infoanime
+┃│ 🌩 .lolice
+┃│ 🌩 .waifu
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄✰⃟⃟き
+┃│ 🌩 .delvn <text>
+┃│ 🌩 .delmsg <text>
+┃│ 🌩 .delimg <text>
+┃│ 🌩 .delsticker <text>
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐅𝐈𝐋𝐓𝐑𝐄𝐒✰⃟⃟き
+┃│ 🌩 .dsowner
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐆𝐑𝐔𝐏𝐎𝐒✰⃟⃟き
+┃│ 🌩 .group abrir / cerrar
+┃│ 🌩 .delete
+┃│ 🌩 .setppgroup
+┃│ 🌩 .rentar2
+┃│ 🌩 .setwelcome
+┃│ 🌩 .demote
+┃│ 🌩 .encuesta <text|text2>
+┃│ 🌩 .hidetag
+┃│ 🌩 .infogrupo
+┃│ 🌩 .invite *<numero>*
+┃│ 🌩 .kick
+┃│ 🌩 .link
+┃│ 🌩 .promote
+┃│ 🌩 .rentar
+┃│ 🌩 .tagall *<mesaje>*
+┃│ 🌩 .invocar *<mesaje>*
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐎𝐍 / 𝐎𝐅𝐅✰⃟⃟き
+┃│ 🌩 .enable <option>
+┃│ 🌩 .disable <option>
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒✰⃟⃟き
+┃│ 🌩 .facebook
+┃│ 🌩 .fb
+┃│ 🌩 .play
+┃│ 🌩 .play2
+┃│ 🌩 .gitclone *<url git>*
+┃│ 🌩 .instagram
+┃│ 🌩 .ig
+┃│ 🌩 .imagen <query>
+┃│ 🌩 .mediafire <url>
+┃│ 🌩 .apk
+┃│ 🌩 .ytmp3doc
+┃│ 🌩 .ytmp4doc
+┃│ 🌩 .spotify
+┃│ 🌩 .tiktok
+┃│ 🌩 .tw
+┃│ 🌩 .ytmp3 *<url youtube>*
+┃│ 🌩 .ytmp4 *<url youtube>*
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐇𝐄𝐑𝐑𝐀𝐌𝐈𝐄𝐍𝐓𝐀𝐒✰⃟⃟き
+┃│ 🌩 .toanime
+┃│ 🌩 .tts <lang> <teks>
+┃│ 🌩 .imagen <query>
+┃│ 🌩 .remini
+┃│ 🌩 .hd
+┃│ 🌩 .enhance
+┃│ 🌩 .nuevafotochannel
+┃│ 🌩 .nosilenciarcanal
+┃│ 🌩 .silenciarcanal
+┃│ 🌩 .noseguircanal
+┃│ 🌩 .seguircanal
+┃│ 🌩 .avisoschannel
+┃│ 🌩 .resiviravisos
+┃│ 🌩 .inspect
+┃│ 🌩 .inspeccionar
+┃│ 🌩 .eliminarfotochannel
+┃│ 🌩 .reactioneschannel
+┃│ 🌩 .reaccioneschannel
+┃│ 🌩 .nuevonombrecanal
+┃│ 🌩 .nuevadescchannel
+┃│ 🌩 .readvo
+┃│ 🌩 .infobot
+┃│ 🌩 .speed
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎́𝐍✰⃟⃟き
+┃│ 🌩 .creador
+┃│ 🌩 .creadoa
+┃│ 🌩 .owner
+┃│ 🌩 .dash
+┃│ 🌩 .dashboard
+┃│ 🌩 .views
+┃│ 🌩 .database
+┃│ 🌩 .usuarios
+┃│ 🌩 .user
+┃│ 🌩 .ds
+┃│ 🌩 .fixmsgespera
+┃│ 🌩 .infobot
+┃│ 🌩 .speed
+┃│ 🌩 .ping
+┃│ 🌩 .sistema
+┃│ 🌩 .speed
+┃│ 🌩 .speedtest
+┃│ 🌩 .groups
+┃│ 🌩 .grouplist
+┃│ 🌩 .reportar
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐍𝐒𝐅𝐖✰⃟⃟き
+┃│ 🌩 .nsfwloli
+┃│ 🌩 .nsfwfoot
+┃│ 🌩 .nsfwass
+┃│ 🌩 .nsfwbdsm
+┃│ 🌩 .nsfwcum
+┃│ 🌩 .nsfwero
+┃│ 🌩 .nsfwfemdom
+┃│ 🌩 .nsfwfoot
+┃│ 🌩 .nsfwglass
+┃│ 🌩 .nsfworgy
+┃│ 🌩 .yuri
+┃│ 🌩 .yuri2
+┃│ 🌩 .yaoi
+┃│ 🌩 .yaoi2
+┃│ 🌩 .panties
+┃│ 🌩 .tetas
+┃│ 🌩 .booty
+┃│ 🌩 .ecchi
+┃│ 🌩 .furro
+┃│ 🌩 .hentai
+┃│ 🌩 .trapito
+┃│ 🌩 .imagenlesbians
+┃│ 🌩 .pene
+┃│ 🌩 .porno
+┃│ 🌩 .randomxxx
+┃│ 🌩 .pechos
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐂𝐑𝐄𝐀𝐃𝐎𝐑✰⃟⃟き
+┃│ 🌩 .enable <option>
+┃│ 🌩 .disable <option>
+┃│ 🌩 .addprem [@user] <days>
+┃│ 🌩 >
+┃│ 🌩 =>
+┃│ 🌩 .copia
+┃│ 🌩 .broadcastgroup <teks>
+┃│ 🌩 .bcgc <teks>
+┃│ 🌩 .bcgc2
+┃│ 🌩 .broadcast <teks>
+┃│ 🌩 .bc <teks>
+┃│ 🌩 .cheat
+┃│ 🌩 .cleartmp
+┃│ 🌩 .delprem <@user>
+┃│ 🌩 .dsowner
+┃│ 🌩 $
+┃│ 🌩 .fetch
+┃│ 🌩 .get
+┃│ 🌩 .getplugin *<nombre>*
+┃│ 🌩 .nuevabiobot <teks>
+┃│ 🌩 .nuevafotobot *<imagen>*
+┃│ 🌩 .nuevonombrebot <teks>
+┃│ 🌩 .prefix [prefix]
+┃│ 🌩 .resetprefix
+┃│ 🌩 .restart
+┃│ 🌩 .saveplugin nombre
+┃│ 🌩 .update
+┃│ 🌩 .actualizar
+┃│ 🌩 >
+┃│ 🌩 =>
+┃│ 🌩 .resetpersonajes
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐒𝐓𝐀𝐅𝐅 ✰⃟⃟き
+┃│ 🌩 .autoadmin
+┃│ 🌩 .banchat
+┃│ 🌩 .banuser <@tag> <razón>
+┃│ 🌩 .grupocrear <nombre>
+┃│ 🌩 .ip <alamat ip>
+┃│ 🌩 .join <link>
+┃│ 🌩 .unbanchat
+┃│ 🌩 .unbanuser <@tag>
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐀𝐔𝐃𝐈𝐎𝐒✰⃟⃟き
+┃│ 🌩 .bass [vn]
+┃│ 🌩 .blown [vn]
+┃│ 🌩 .deep [vn]
+┃│ 🌩 .earrape [vn]
+┃│ 🌩 .fast [vn]
+┃│ 🌩 .fat [vn]
+┃│ 🌩 .nightcore [vn]
+┃│ 🌩 .reverse [vn]
+┃│ 🌩 .robot [vn]
+┃│ 🌩 .slow [vn]
+┃│ 🌩 .smooth [vn]
+┃│ 🌩 .tupai [vn]
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥  𝐀 𝐈✰⃟⃟き
+┃│ 🌩 .chatgpt <texto>
+┃│ 🌩 .ia <texto>
+╰╦═════════════ ∆••••••∆
+╭┤き⃟🔥 𝐂𝐎𝐍𝐕𝐄𝐑𝐓𝐈𝐃𝐎𝐑𝐄𝐒✰⃟⃟き
+┃│ 🌩 .togifaud
+┃│ 🌩 .tourl
+┃│ 🌩 .tovideo
+╰══════════════ ∆••••••∆
 `
  await conn.sendMessage(m.chat, {
       image: { url: 'https://qu.ax/HGqex.jpg' },
@@ -11,9 +374,9 @@ let texto = `
       footer: botname,
       buttons: [
         {
-          buttonId: `.menucompleto`,
+          buttonId: `.infobot`,
           buttonText: {
-            displayText: 'MENU COMPLETO',
+            displayText: 'INFO BOT',
           },
         },
         {
